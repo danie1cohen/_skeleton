1040f4264e2922e1b8a4766c4ae57950aa9601c9
Dan Cohen <dcohen@usccreditunion.org>
2016-05-09 09:29:41 -0700

    Use print function (make friendly for python3)

--- _skeleton/tests/_skeleton_tests.py
+++ _skeleton/tests/_skeleton_tests.py
@@ -3,6 +3,7 @@ Tests for _skeleton module
 """
 # pylint: disable=missing-docstring, import-error, wildcard-import
 # pylint: disable=attribute-defined-outside-init,unused-wildcard-import, no-init
+from __future__ import print_function
 
 from nose.tools import *
 
@@ -11,10 +12,10 @@ import _skeleton
 
 class Test_skeleton(object):
     def setup(self):
-        print 'SETUP!'
+        print('SETUP!')
 
     def teardown(self):
-        print 'TEAR DOWN!'
+        print('TEAR DOWN!')
 
     def test_basic(self):
-        print 'I RAN!'
+        print('I RAN!')
