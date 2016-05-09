03dccdf3d6a73ef387c083c4978993f64168667f
Dan Cohen <dcohen@usccreditunion.org>
2016-05-09 09:28:32 -0700

    Remove unnecssary unicode string note which breaks python3 installs

--- _skeleton/setup.py
+++ _skeleton/setup.py
@@ -6,16 +6,16 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    u'description': u'_skeleton',
-    u'author': u'_user_name',
-    u'url': u'_download_url',
-    u'download_url': u'_download_url',
-    u'author_email': u'_user_email',
-    u'version': u'0.1',
-    u'install_requires': [u'nose'],
-    u'packages': [u'_skeleton'],
-    u'scripts': [],
-    u'name': u'_skeleton'
+    'description': '_skeleton',
+    'author': '_user_name',
+    'url': '_download_url',
+    'download_url': '_download_url',
+    'author_email': '_user_email',
+    'version': '0.1.0',
+    'install_requires': [],
+    'packages': ['_skeleton'],
+    'scripts': [],
+    'name': '_skeleton'
 }
 
 setup(**config)
