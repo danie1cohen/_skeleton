f423d12c2defd6ebb316de9827acd66605a10a7d
Dan Cohen <dcohen@usccreditunion.org>
2017-12-01 10:48:00 -0800

    Update pip and specify ansible version

--- _skeleton/bootstrap.sh
+++ _skeleton/bootstrap.sh
@@ -3,10 +3,6 @@ export TZ=America/Los_Angeles
 export DEBIAN_FRONTEND=noninteractive
 
 echo "
-# aliases
-alias provision=\"cd /vagrant/playbook && ansible-playbook dev.yml --vault-password-file ~/.vault\"
-alias deploy=\"cd /vagrant/playbook && ansible-playbook prod.yml --vault-password-file ~/.vault\"
-
 cd /vagrant/
 workon _skeleton" > /home/vagrant/.profile
 
@@ -15,13 +11,14 @@ find /home/vagrant/ -name .vault -exec rm {} \\;
 find /vagrant/ -name \"*.retry\" -exec rm {} \\;
 " >> /home/vagrant/.logout
 
-echo "source ~/.logout" >> ~/.bash_logout
+echo "source ~/.logout" >> /home/vagrant/.bash_logout
 
 
 apt-get update
 apt-get -y install vim tree build-essential libssl-dev libffi-dev python-dev python-pip
 apt-get -y --no-install-recommends install git-core
 
-pip install ansible==2.3.2 markupsafe
+pip install -U pip
+pip install ansible==2.3.2
 
 chown -R vagrant:vagrant /home/vagrant
